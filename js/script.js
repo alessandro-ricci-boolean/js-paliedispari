8f8be92cea16eeae22069079a922fb99cb378db6
alessandro-ricci-boolean <cqfp.official@gmail.com>
2023-11-29 19:46:31 +0100

    added pari e dispari exercise

--- js/script.js
+++ js/script.js
@@ -12,3 +12,32 @@ function checkPalindroma(word){
 let string = prompt("Inserisci una parola")
 checkPalindroma(string);
 
+// PARI E DISPARI
+
+function generateRandomNumber(){
+    return Math.floor(Math.random() * 5 + 1);
+}
+
+function checkSomma(sum){
+    if(sum % 2 == 0){
+        return "pari";
+    }
+    return "dispari";
+}
+
+let choice = prompt("Inserisci un valore tra pari e dispari");
+
+let number = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
+let pc_number = generateRandomNumber();
+
+let somma = number + pc_number;
+
+let result = checkSomma(somma);
+
+if(result == "choice"){
+    alert("Hai vinto");
+}
+else{
+    alert("Hai perso");
+}
